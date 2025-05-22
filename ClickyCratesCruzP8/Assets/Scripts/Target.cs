c1da1eedb0e6a2011b483adcc4b00f865978348f
Josecruz777 <145380670+cdblast@users.noreply.github.com>
2025-05-22 15:26:21 -0700

    Clicky Crates
    i made the crates have colerful explosions, made game over text, and i also made the restart text.

--- ClickyCratesCruzP8/Assets/Scripts/Target.cs
+++ ClickyCratesCruzP8/Assets/Scripts/Target.cs
@@ -12,6 +12,7 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -2;
 
+    public ParticleSystem explosionParticle;
     public int pointValue;
 
     // Start is called before the first frame update
@@ -34,15 +35,23 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(pointValue);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
-    
 
 
     Vector3 RandomForce()
